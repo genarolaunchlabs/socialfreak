f8e0c9c77fb1f72fd7d4f21e88c48342f79a26a9
Genaro Legaspi <genaro_legaspi@launchlabs.ph>
2016-04-26 16:37:33 +0800

    Fixed the auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
+require 'yaml'
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['50sox7mvEKpfWcNC6siYvCzsG'], ENV['1I2COpYoNab9O4UY4PN29bk1ms0HzGBR4cErI5gGAKHqPDf0aX']
+  provider :twitter, Rails.application.secrets.twitter_api_key, Rails.application.secrets.twitter_api_secret
 end
